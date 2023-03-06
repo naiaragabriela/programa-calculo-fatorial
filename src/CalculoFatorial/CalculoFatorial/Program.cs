1066365983bb0a1f9934ddeb37f3e41e4265c277
naiaragabriela <naigabi31@gmail.com>
2023-03-05 21:24:14 -0300

    calculo de faotrial usando função

---
+++ src/CalculoFatorial/CalculoFatorial/Program.cs
@@ -0,0 +1,29 @@
+﻿int anterior, numero, fatorial;
+
+int BuscaNumero()
+{
+    Console.Write("Informe um número: ");
+    return int.Parse(Console.ReadLine());
+}
+
+numero = BuscaNumero();
+anterior = (numero - 1);
+fatorial = 1;
+
+void ResultadoFatorial()
+{
+    for (int i = numero; i>1; i--)
+    {
+        fatorial *= numero;
+        Console.WriteLine($"{numero}! = {numero} * {anterior}!");
+        numero = anterior;
+        anterior = anterior - 1;
+
+        if(numero == 1 || numero == 0)
+        {
+            Console.WriteLine($"{numero}! = 1");
+        }
+    }
+    Console.WriteLine($"O resultado do fatorial é: {fatorial}");
+}
+ResultadoFatorial();
